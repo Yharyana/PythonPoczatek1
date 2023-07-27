6126d11d25a9027e73f108ea56d9208f0637165b
Jakub Zbroiński <kubazbroinsk@gmail.com>
2023-07-27 13:42:36 +0200

    dziala

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello PYthon Początek")
+import testodin
+#import  math
+#print("hello PYthon Początek")
+##bawie sie debuggerem
+#num=6
+##num+=1
+#num+=1
+#num+=1
+#num+=1
+#num=math.pow(num,2)
+#if num>2:
+##    print("DONKI KONG")
+#print(num)
+#num+=1
+testodin.sayhello()
